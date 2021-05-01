e871e86547dc09b3c43c5d35931a86b850bfb615
MikeShahin <gobias101@gmail.com>
2021-05-01 14:24:34 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your functions here
+function map(sourceArray, callback) {
+    const newArray = [];
+
+    for (let i=0; i < sourceArray.length; i++) {
+        let x = sourceArray[i];
+        newArray.push(callback(x));
+    };
+    return newArray;
+}
+
+function reduce(sourceArray, callback, startPoint) {
+    let total;
+    if (startPoint) {
+        total = startPoint;
+        for (let i=0; i < sourceArray.length; i++) {
+            total = callback(sourceArray[i], total);
+        }
+        return total
+    } else {
+        total = sourceArray[0];
+        for (let i=1; i < sourceArray.length; i++) {
+            total = callback(sourceArray[i], total)
+        }
+        return total
+    }
+};
